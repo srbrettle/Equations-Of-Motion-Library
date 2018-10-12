c91691a5ba92682ddf08512452d0fa3df4a0720b
Scott Brettle <srbrettle@gmail.com>
2018-10-12 22:45:58 +0100

    Added TimeFromSVA function and non-quadratic TimeFromSUA function.

--- EquationsOfMotion/Time.cs
+++ EquationsOfMotion/Time.cs
@@ -14,12 +14,24 @@ namespace EquationsOfMotion
             return displacement / (0.5 * (initialVelocity + finalVelocity));
         }
 
-        public static double[] TimeFromSUA(double displacement, double initialVelocity, double acceleration)
+        public static double[] TimeFromSUAQuadratic(double displacement, double initialVelocity, double acceleration)
         {
             double[] time = new double[2];
             time[0] = (-initialVelocity + Math.Sqrt(Math.Pow(initialVelocity, 2) + 2 * acceleration * displacement)) / acceleration;
             time[1] = (-initialVelocity - Math.Sqrt(Math.Pow(initialVelocity, 2) + 2 * acceleration * displacement)) / acceleration;
             return time;
+        }
+
+        public static double TimeFromSUA(double displacement, double initialVelocity, double acceleration)
+        {
+            double finalVelocity = FinalVelocity.FinalVelocityFromSUA(displacement, initialVelocity, acceleration);
+            return TimefromUVA(initialVelocity, finalVelocity, acceleration);
+        }
+
+        public static double TimeFromSVA(double displacement, double finalVelocity, double acceleration)
+        {
+            double initialVelocity = InitialVelocity.InitialVelocityFromSVA(displacement, finalVelocity, acceleration);
+            return TimefromUVA(initialVelocity, finalVelocity, acceleration);
         }
     }
 }
