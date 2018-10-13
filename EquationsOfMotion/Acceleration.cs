9261e6c519c374a8737726d7af25bbc6ec58690f
Scott Brettle <srbrettle@gmail.com>
2018-10-13 11:14:05 +0100

    Added XML Documentation for Acceleration

--- EquationsOfMotion/Acceleration.cs
+++ EquationsOfMotion/Acceleration.cs
@@ -2,24 +2,55 @@
 
 namespace EquationsOfMotion
 {
+    /// <summary>
+    /// Equations for Constant Acceleration
+    /// </summary>
     public static class Acceleration
     {
-        public static double AccelerationsFromUVT(double initialVelocity, double finalVelocity, double time)
+        /// <summary>
+        /// Calculates Acceleration from Initial Velocity, Final Velocity and Time.
+        /// </summary>
+        /// <param name="initialVelocity">Initial Velocity</param>
+        /// <param name="finalVelocity">Final Velocity</param>
+        /// <param name="time">Time</param>
+        /// <returns>Acceleration</returns>
+        public static double AccelerationFromUVT(double initialVelocity, double finalVelocity, double time)
         {
             return (finalVelocity - initialVelocity) / time;
         }
 
-        public static double AccelerationsFromSUV(double displacement, double initialVelocity, double finalVelocity)
+        /// <summary>
+        /// Calculates Acceleration from Displacement, Initial Velocity and Final Velocity.
+        /// </summary>
+        /// <param name="displacement">Displacement</param>
+        /// <param name="initialVelocity">Initial Velocity</param>
+        /// <param name="finalVelocity">Final Velocity</param>
+        /// <returns>Acceleration</returns>
+        public static double AccelerationFromSUV(double displacement, double initialVelocity, double finalVelocity)
         {
             return (Math.Pow(finalVelocity, 2) - Math.Pow(initialVelocity, 2)) / (2 * displacement);
         }
 
-        public static double AccelerationsFromSUT(double displacement, double initialVelocity, double time)
+        /// <summary>
+        /// Calculates Acceleration from Displacement, Initial Velocity and Time.
+        /// </summary>
+        /// <param name="displacement">Displacement</param>
+        /// <param name="initialVelocity">Initial Velocity</param>
+        /// <param name="time">Time</param>
+        /// <returns>Acceleration</returns>
+        public static double AccelerationFromSUT(double displacement, double initialVelocity, double time)
         {
             return (2 * (displacement - initialVelocity * time)) / Math.Pow(time, 2);
         }
 
-        public static double AccelerationsFromSVT(double displacement, double finalVelocity, double time)
+        /// <summary>
+        /// Calculates Acceleration from Displacement, Final Velocity and Time.
+        /// </summary>
+        /// <param name="displacement">Displacement</param>
+        /// <param name="finalVelocity">Final Velocity</param>
+        /// <param name="time">Time</param>
+        /// <returns>Acceleration</returns>
+        public static double AccelerationFromSVT(double displacement, double finalVelocity, double time)
         {
             return (2 * (finalVelocity * time - displacement) / Math.Pow(time, 2));
         }
