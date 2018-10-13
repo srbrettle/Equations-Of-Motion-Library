da9f59dff4dc851881f33c11067bf8b0e0c4d5cc
Scott Brettle <srbrettle@gmail.com>
2018-10-13 10:35:22 +0100

    Added Acceleration Unit Tests.

--- UnitTest.EquationsOfMotion/Test.Acceleration.cs
+++ UnitTest.EquationsOfMotion/Test.Acceleration.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
+using static EquationsOfMotion.Acceleration;
 
 namespace UnitTest.EquationsOfMotion
 {
     public class Acceleration
     {
         [Fact]
-        public void Test1()
+        public void Test_AccelerationsFromUVT()
         {
+            double result = AccelerationsFromUVT(10, 20, 5);
+            Assert.Equal(2, result);
+        }
 
+        [Fact]
+        public void Test_AccelerationsFromSUV()
+        {
+            double result = AccelerationsFromSUV(75, 10, 20);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test_AccelerationsFromSUT()
+        {
+            double result = AccelerationsFromSUT(75, 10, 5);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test_AccelerationsFromSVT()
+        {
+            double result = AccelerationsFromSVT(75, 20, 5);
+            Assert.Equal(2, result);
         }
     }
 }
