d620bfbf06db9862d56253dea452cfc24f3ad89d
Scott Brettle <srbrettle@gmail.com>
2018-10-13 10:47:53 +0100

    Added Initial Velocity Tests.

--- UnitTest.EquationsOfMotion/Test.InitialVelocity.cs
+++ UnitTest.EquationsOfMotion/Test.InitialVelocity.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
+using static EquationsOfMotion.InitialVelocity;
 
 namespace UnitTest.EquationsOfMotion
 {
     public class InitialVelocity
     {
         [Fact]
-        public void Test1()
+        public void Test_InitialVelocityFromSAT()
         {
+            double result = InitialVelocityFromSAT(75, 2, 5);
+            Assert.Equal(10, result);
+        }
 
+        [Fact]
+        public void Test_InitialVelocityFromSVT()
+        {
+            double result = InitialVelocityFromSVT(75, 20, 5);
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void Test_InitialVelocityFromSVA()
+        {
+            double result = InitialVelocityFromSVA(75, 20, 2);
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void Test_InitialVelocityFromVAT()
+        {
+            double result = InitialVelocityFromVAT(20, 2, 5);
+            Assert.Equal(10, result);
         }
     }
 }
