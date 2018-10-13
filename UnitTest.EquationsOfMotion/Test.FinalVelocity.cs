8f06e9105365fb9669d2fd59eced5fc198218ff4
Scott Brettle <srbrettle@gmail.com>
2018-10-13 10:41:33 +0100

    Added Final Velocity Tests.

--- UnitTest.EquationsOfMotion/Test.FinalVelocity.cs
+++ UnitTest.EquationsOfMotion/Test.FinalVelocity.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
+using static EquationsOfMotion.FinalVelocity;
 
 namespace UnitTest.EquationsOfMotion
 {
     public class FinalVelocity
     {
         [Fact]
-        public void Test1()
+        public void Test_FinalVelocityFromUAT()
         {
+            double result = FinalVelocityFromUAT(10, 2, 5);
+            Assert.Equal(20, result);
+        }
 
+        [Fact]
+        public void Test_FinalVelocityFromSUA()
+        {
+            double result = FinalVelocityFromSUA(75, 10, 2);
+            Assert.Equal(20, result);
+        }
+
+        [Fact]
+        public void Test_FinalVelocityFromSUT()
+        {
+            double result = FinalVelocityFromSUT(75, 10, 5);
+            Assert.Equal(20, result);
+        }
+
+        [Fact]
+        public void Test_FinalVelocityFromSAT()
+        {
+            double result = FinalVelocityFromSAT(75, 2, 5);
+            Assert.Equal(20, result);
         }
     }
 }
